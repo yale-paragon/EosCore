6852e6e1a8a7df4974a16cc28e70b59dfa06d6c0
emmethoughton <emmet.houghton@yale.edu>
2024-08-05 10:15:00 -0400

    Update emmetcore.v
    latest stable version

--- emmetcore_bridge.srcs/sources_1/new/emmetcore.v
+++ emmetcore_bridge.srcs/sources_1/new/emmetcore.v
@@ -1,4 +1,3 @@
-
 `timescale 1ns / 1ps
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -14,19 +13,21 @@
 // Description: Low-latency transceiver module for 64B/66B encoding
 //
 // Some notes on using this core:
-// - The core resets while tx_corereset_n is *low* so that it resets upon initialization automatically.
+// - Recommended line rate: 16 Gbps
+// - The core resets while tx_corereset_n is *low* so that it resets upon initialization automatically. Expect delay during reset.
 // - The code assumes both partners are set to the same line rate (so that tx_userclk and rx_userclk have the same frequencies on each end).
-// - Expect a delay of roughly 1500 cycles of {tx/rx}_userclk upon each reset. This delay ensures both gearboxes are aligned properly.
 // - An early error flag (within ~128 cycles after streaming begins) suggests all data is likely invalid.
 // - A later error flag could suggest a gearbox drift, partner shutdown, or a bit flip. Try resetting and re-attempt.
 // - If streaming doesn't resume after boards are reprogrammed, lower tx_corereset_n for at least one cycle.
+//
+//////////////////////////////////////////////////////////////////////////////////
 
 module emmetcore(
     // Reset signal (active low)
     input tx_corereset_n,
     // Separate, slower clock for initialization
     input init_clk,
-    
+   
     // Transceiver primitives
     input tx_userclk,
     input rx_userclk,
@@ -38,7 +39,7 @@ module emmetcore(
     input [63:0] tx_data,
     input tx_valid,
     output reg tx_ready,
-    
+   
     // TX Lane
     output reg [63:0] tx_userdata_out,
     output reg [1:0] tx_header_out,
@@ -51,37 +52,37 @@ module emmetcore(
     input rx_headervalid_in,
     output reg rx_gearboxslip,
     output reg rx_polarity,
-    
+   
     // RX End
     output reg [63:0] rx_data,
     output reg rx_valid,
     output reg error_flag
     );
          
-          
+         
     // Reset for at least 512 cycles to ensure gearbox alignment
     reg [9:0] reset_cycle_count;
-    // Registers to lock when reset is complete. Need extra registers to cross clock domains     
+    // Registers to lock when reset is complete. Need extra registers to cross clock domains    
     reg resetstate_n_initclk;
     reg resetstate_n_intermediate;
     reg resetstate_n_txuserclk;
     // Raised when this core's gearbox is aligned
     reg coreresetdone_rxclk;
     reg coreresetdone_txclkmeta;  
-    reg coreresetdone_txclkstable; 
+    reg coreresetdone_txclkstable;
     // Raised when partner's gearbox is aligned
     reg partner_coreresetdone_rxclk;    
-    reg partner_coreresetdone_txclkmeta; 
-    reg partner_coreresetdone_txclkstable; 
-                  
+    reg partner_coreresetdone_txclkmeta;
+    reg partner_coreresetdone_txclkstable;
+                 
     // Some initialization logic from transceivers must operate on separate clock for timing purposes (too slow for GT clocks)                  
     always @(posedge init_clk) begin
         // Initiate a reset under a set of conditions
         if (!tx_corereset_n || !tx_resetdone || !rx_resetdone) resetstate_n_initclk <= 0;
         else if (!resetstate_n_initclk) resetstate_n_initclk <= 1;
-    end         
-    
-    
+    end        
+   
+   
     // Note: functionality could be combined to use only 2 of these keys, but I keep them distinct for debugging purposes.
     // Initial random seed for LFSR (32x1, 32x0)
     localparam [63:0] LFSR_SEED = 64'hE9734555354A526D;
@@ -98,28 +99,28 @@ module emmetcore(
     reg [63:0] lfsr_reg;
     wire feedback;
     assign feedback = lfsr_reg[63] ^ lfsr_reg[62] ^ lfsr_reg[60] ^ lfsr_reg[59];
-    
+   
     // Generates next LFSR sequence and passes it to TX lane
     task cycle_lfsr;
-        if (lfsr_reg == 64'b0) begin 
+        if (lfsr_reg == 64'b0) begin
             lfsr_reg <= LFSR_SEED;
             tx_userdata_out <= LFSR_SEED;
         end
         else begin
-            lfsr_reg <= {lfsr_reg[62:0], feedback}; 
+            lfsr_reg <= {lfsr_reg[62:0], feedback};
             tx_userdata_out <= lfsr_reg ^ SCRAMBLE_KEY;
         end
     endtask
-    
-    
+   
+   
     // Bitwise parity counters for weak error detection
     reg [63:0] tx_parity_reg;
     reg [63:0] rx_parity_reg;
-    
+   
     // Sequence increments every *other* cycle
     reg increment_sequence;
        
-    
+   
     // TX Processing
     always @(posedge tx_userclk) begin
         // Stabilize cross-clock domain registers
@@ -128,14 +129,14 @@ module emmetcore(
         coreresetdone_txclkstable <= coreresetdone_txclkmeta;
         partner_coreresetdone_txclkmeta <= partner_coreresetdone_rxclk;
         partner_coreresetdone_txclkstable <= partner_coreresetdone_txclkmeta;
-        
+       
         // Sequence
         increment_sequence <= !increment_sequence;
         if (increment_sequence) begin
             if (tx_sequence_out >= 7'h20) tx_sequence_out <= 0;
             else tx_sequence_out <= tx_sequence_out + 1;
         end
-                
+               
         // Reset state
         if (!resetstate_n_txuserclk || !coreresetdone_txclkstable || !partner_coreresetdone_txclkstable || reset_cycle_count > 0) begin
             tx_ready <= 0;
@@ -156,7 +157,7 @@ module emmetcore(
             // In between, send semi-random sequences with control headers
             else begin
                 reset_cycle_count <= reset_cycle_count + 1;
-                if (reset_cycle_count == 64'b0) begin
+                if (reset_cycle_count == 64'b0 && !coreresetdone_txclkstable) begin
                     tx_header_out <= 2'b00;
                     tx_userdata_out <= RESET_SIGNAL;
                 end
@@ -166,19 +167,19 @@ module emmetcore(
                 end
             end
             tx_parity_reg <= 64'b0;
-        end 
-        
+        end
+       
         else begin
             // Prepare/synchronize reset registers
             resetstate_n_txuserclk <= resetstate_n_intermediate;
             reset_cycle_count <= 0;
-            
+           
             // Streaming state
             if (gt_powergood) begin
                 // Cannot accept data during sequence pause
                 if (tx_sequence_out == 7'h1f || (tx_sequence_out == 7'h20 && !increment_sequence)) tx_ready <= 0;
                 else tx_ready <= 1;
-                
+               
                 // Handshake with user program, scramble and pass data to TX lane, update bitwise parity counts
                 if (tx_valid && tx_ready) begin
                     tx_userdata_out <= tx_data ^ SCRAMBLE_KEY;
@@ -190,7 +191,7 @@ module emmetcore(
                         tx_userdata_out <= tx_parity_reg ^ PARITY_KEY;
                         tx_header_out <= 2'b01;
                         tx_parity_reg <= 64'b0;                        
-                    end else begin 
+                    end else begin
                         cycle_lfsr();
                         tx_header_out <= 2'b01;
                     end
@@ -203,24 +204,24 @@ module emmetcore(
                 tx_header_out <= 2'b01;
                 tx_parity_reg <= 64'b0;
                 tx_ready <= 0;
-            end 
+            end
         end
     end
-    
-    
-    // Determining if partner shuts down
+   
+   
+    // Determining if partner shuts down. Typically, when a board shuts down, it sents ...101010... or ...111111... across the link
     reg [1:0] prev_header;
     reg [63:0] prev_data;
     reg [9:0] consecutiveidenticalblock_count;
-    
+   
     // Need many healthy cycles to confirm gearbox alignment
     reg [6:0] gearboxaligned_count;
-    
+   
     // RX processing
     always @(posedge rx_userclk) begin  
         if (gt_powergood) begin
             if (rx_headervalid_in == rx_datavalid_in) begin
-            
+           
                 // Check to see if partner shut down. If so, wait in reset state
                 if (rx_datavalid_in) begin
                     prev_header <= rx_header_in;
@@ -233,8 +234,8 @@ module emmetcore(
                     coreresetdone_rxclk <= 0;
                     partner_coreresetdone_rxclk <= 0;
                     error_flag <= 1;
-                end 
-            
+                end
+           
                 else case (rx_header_in)
                     // Data header
                     2'b10: begin
@@ -260,8 +261,8 @@ module emmetcore(
                         // Parity check for error detection
                         if (rx_headervalid_in && rx_valid && rx_parity_reg != (rx_userdata_in ^ PARITY_KEY)) error_flag <= 1;
                         else error_flag <= 0;
-                        if (rx_headervalid_in) rx_parity_reg <= 0; 
-                    end 
+                        if (rx_headervalid_in) rx_parity_reg <= 0;
+                    end
                     // 2'b00 and 2'b11 are unused
                     default begin
                         if (rx_headervalid_in) begin
@@ -276,12 +277,14 @@ module emmetcore(
                                 rx_polarity <= !rx_polarity;
                                 rx_gearboxslip <= 0;
                                 if (partner_coreresetdone_rxclk) begin
+                                    coreresetdone_rxclk <= 0;
                                     partner_coreresetdone_rxclk <= 0;
                                     error_flag <= 1;
                                 end
                             end else if (rx_header_in == 2'b00 && rx_userdata_in == RESET_SIGNAL) begin
                                 rx_gearboxslip <= 0;
                                 if (partner_coreresetdone_rxclk) begin
+                                    coreresetdone_rxclk <= 0;
                                     partner_coreresetdone_rxclk <= 0;
                                     error_flag <= 1;
                                 end
@@ -298,6 +301,5 @@ module emmetcore(
             end else error_flag <= 1;
         end
     end
-    
+   
 endmodule
-
